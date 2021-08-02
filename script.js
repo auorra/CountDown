ba9a903fa801d3117eee8bb10f5591575012fa36
auorra <auora2020@protonmail.com>
2021-08-01 20:42:02 -0700

    new commit

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const coutndownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
 //Set Date input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
@@ -15,6 +21,9 @@ function updateCountDown(e) {
 	countdownTitle = e.srcElement[0].value;
 	countdownDate = e.srcElement[1].value;
 	console.log(countdownTitle, countdownDate)
+	//Get number version of current Date, updateDOM
+	countdownValue = new Date(countdownDate).getTime();
+	console.log('countdown value: ', countdownValue);
 
 }
 
